ca8dbea90eef5c08f54135999ca59bfd935e361a
volu1275 <kamala.kannan.chellapandi@uni-weimar.de>
2014-06-23 19:14:36 +0200

    added

---
+++ CLEyeFaceTracker/stdafx.h
@@ -0,0 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// This file is part of CL-EyeMulticam SDK
+//
+// C++ Sample Application
+//
+// It allows the use of multiple CL-Eye cameras in your own applications
+//
+// For updates and file downloads go to: http://codelaboratories.com/research/view/cl-eye-muticamera-sdk
+//
+// Copyright 2008-2012 (c) Code Laboratories, Inc. All rights reserved.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+//#include "..\global.h"
+#include <stdio.h>
+#include <conio.h>
+#include "CLEyeMulticam.h"
+#include <windows.h>
+#include <tchar.h>
+#include <opencv2/opencv.hpp>
